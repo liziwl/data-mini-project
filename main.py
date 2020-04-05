593c7336a2b0acd4ddc51739e02688051d0deeee
liziwl <leezisy@gmail.com>
2020-04-05 10:52:42 +0800

    update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# %%
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
@@ -8,44 +9,46 @@ from sklearn.metrics import roc_curve, auc
 train_data_ = pd.read_csv('dataset/train.data', sep='\s+', header=None)
 test_data_ = pd.read_csv('dataset/test.data', sep='\s+', header=None)
 
+train_data_.rename(columns={10:'target'},inplace=True)
+test_data_.rename(columns={10:'target'},inplace=True)
 
-# train_data_.iloc[:,-1].value_counts()
-# -1    7819
-#  1     466
-# Name: 10, dtype: int64
-# -1 0.9437537718768859
-# 1 0.05624622812311406
-# 所以要考虑数据的分布
+# %%
+target_count = train_data_.target.value_counts()
+print('Class -1:', target_count[-1])
+print('Class 1:', target_count[1])
+print('Proportion:', round(target_count[-1] / target_count[1], 2), ': 1')
+print(f'Rate: {target_count[-1]/(target_count[-1] + target_count[1])*100}%')
+
+target_count.plot(kind='bar', title='Count (target)')
+
+# %%
+count_class_0, count_class_1 = train_data_.target.value_counts()
+
+#%%
+
+# Divide by class
+df_class_0 = train_data_[train_data_.target == -1]
+df_class_1 = train_data_[train_data_.target == 1]
+
+#%%
+df_class_0_under = df_class_0.sample(count_class_1)
+df_test_under = pd.concat([df_class_0_under, df_class_1], axis=0)
+
+# 欠采样
+print('Random under-sampling:')
+print(df_test_under.target.value_counts())
+
+df_test_under.target.value_counts().plot(kind='bar', title='Count (target)')
+
+#%%
+
+# 超采样
+df_class_1_over = df_class_1.sample(count_class_0, replace=True)
+df_test_over = pd.concat([df_class_0, df_class_1_over], axis=0)
+
+print('Random over-sampling:')
+print(df_test_over.target.value_counts())
+
+df_test_over.target.value_counts().plot(kind='bar', title='Count (target)');
 
 
-def acu_curve(y, prob):
-    fpr, tpr, threshold = roc_curve(y, prob)  ###计算真正率和假正率
-    roc_auc = auc(fpr, tpr)  ###计算auc的值
-
-    plt.figure()
-    lw = 2
-    plt.figure(figsize=(10, 10))
-    plt.plot(fpr, tpr, color='darkorange',
-             lw=lw, label='ROC curve (area = %0.3f)' % roc_auc)  ###假正率为横坐标，真正率为纵坐标做曲线
-    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver operating characteristic example')
-    plt.legend(loc="lower right")
-
-    plt.show()
-
-
-train_data = train_data_.iloc[:, :-1]
-train_target = train_data_.iloc[:, -1]
-
-X_train, X_test, y_train, y_test = train_test_split(train_data, train_target, test_size=0.4, random_state=42)
-
-rf = RandomForestClassifier(oob_score=True, random_state=42)
-rf.fit(X_train, y_train)
-print(rf.oob_score_)
-
-_prob_y = rf.predict(X_test)
-acu_curve(y_test, _prob_y)
