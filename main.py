94c516874d73ff1ad1836552a1e23f1130d2678d
liziwl <leezisy@gmail.com>
2020-04-05 11:57:23 +0800

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import auc, f1_score, precision_score, recall_score, roc_auc_score, roc_curve
 
 train_data_ = pd.read_csv('dataset/train.data', sep='\s+', header=None)
 test_data_ = pd.read_csv('dataset/test.data', sep='\s+', header=None)
@@ -41,14 +41,303 @@ print(df_test_under.target.value_counts())
 df_test_under.target.value_counts().plot(kind='bar', title='Count (target)')
 
 #%%
+new_df = df_test_under.sample(frac=1, random_state=42)
 
-# 超采样
-df_class_1_over = df_class_1.sample(count_class_0, replace=True)
-df_test_over = pd.concat([df_class_0, df_class_1_over], axis=0)
+new_df.head()
 
-print('Random over-sampling:')
-print(df_test_over.target.value_counts())
+# %%
 
-df_test_over.target.value_counts().plot(kind='bar', title='Count (target)');
+X = new_df.drop('target', axis=1)
+y = new_df['target']
+
+y
+
+# %%
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+
+# This is explicitly used for undersampling.
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# %%
+
+X_train = X_train.values
+X_test = X_test.values
+y_train = y_train.values
+y_test = y_test.values
+
+# %%
+
+# Let's implement simple classifiers
+
+classifiers = {
+    "LogisiticRegression": LogisticRegression(),
+    "KNearest": KNeighborsClassifier(),
+    "Support Vector Classifier": SVC(),
+    "DecisionTreeClassifier": DecisionTreeClassifier()
+}
+
+# %%
+
+# Wow our scores are getting even high scores even when applying cross validation.
+from sklearn.model_selection import cross_val_score
 
 
+for key, classifier in classifiers.items():
+    classifier.fit(X_train, y_train)
+    training_score = cross_val_score(classifier, X_train, y_train, cv=5)
+    print("Classifiers: ", classifier.__class__.__name__, "Has a training score of", round(training_score.mean(), 2) * 100, "% accuracy score")
+
+# %%
+
+# Use GridSearchCV to find the best parameters.
+from sklearn.model_selection import GridSearchCV
+
+
+# Logistic Regression 
+log_reg_params = {"penalty": ['l1', 'l2'], 'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+
+
+
+grid_log_reg = GridSearchCV(LogisticRegression(), log_reg_params)
+grid_log_reg.fit(X_train, y_train)
+# We automatically get the logistic regression with the best parameters.
+log_reg = grid_log_reg.best_estimator_
+
+knears_params = {"n_neighbors": list(range(2,5,1)), 'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']}
+
+grid_knears = GridSearchCV(KNeighborsClassifier(), knears_params)
+grid_knears.fit(X_train, y_train)
+# KNears best estimator
+knears_neighbors = grid_knears.best_estimator_
+
+# Support Vector Classifier
+svc_params = {'C': [0.5, 0.7, 0.9, 1], 'kernel': ['rbf', 'poly', 'sigmoid', 'linear']}
+grid_svc = GridSearchCV(SVC(), svc_params)
+grid_svc.fit(X_train, y_train)
+
+# SVC best estimator
+svc = grid_svc.best_estimator_
+
+# DecisionTree Classifier
+tree_params = {"criterion": ["gini", "entropy"], "max_depth": list(range(2,4,1)), 
+              "min_samples_leaf": list(range(5,7,1))}
+grid_tree = GridSearchCV(DecisionTreeClassifier(), tree_params)
+grid_tree.fit(X_train, y_train)
+
+# tree best estimator
+tree_clf = grid_tree.best_estimator_
+
+
+# %%
+
+# Overfitting Case
+
+log_reg_score = cross_val_score(log_reg, X_train, y_train, cv=5)
+print('Logistic Regression Cross Validation Score: ', round(log_reg_score.mean() * 100, 2).astype(str) + '%')
+
+
+knears_score = cross_val_score(knears_neighbors, X_train, y_train, cv=5)
+print('Knears Neighbors Cross Validation Score', round(knears_score.mean() * 100, 2).astype(str) + '%')
+
+svc_score = cross_val_score(svc, X_train, y_train, cv=5)
+print('Support Vector Classifier Cross Validation Score', round(svc_score.mean() * 100, 2).astype(str) + '%')
+
+tree_score = cross_val_score(tree_clf, X_train, y_train, cv=5)
+print('DecisionTree Classifier Cross Validation Score', round(tree_score.mean() * 100, 2).astype(str) + '%')
+
+#%%
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
+
+print('No Frauds', round(train_data_['target'].value_counts()[-1]/len(train_data_) * 100,2), '% of the dataset')
+print('Frauds', round(train_data_['target'].value_counts()[1]/len(train_data_) * 100,2), '% of the dataset')
+
+X = train_data_.drop('target', axis=1)
+y = train_data_['target']
+
+sss = StratifiedKFold(n_splits=5, random_state=None, shuffle=False)
+
+for train_index, test_index in sss.split(X, y):
+    print("Train:", train_index, "Test:", test_index)
+    original_Xtrain, original_Xtest = X.iloc[train_index], X.iloc[test_index]
+    original_ytrain, original_ytest = y.iloc[train_index], y.iloc[test_index]
+
+# We already have X_train and y_train for undersample data thats why I am using original to distinguish and to not overwrite these variables.
+# original_Xtrain, original_Xtest, original_ytrain, original_ytest = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Check the Distribution of the labels
+
+
+# Turn into an array
+original_Xtrain = original_Xtrain.values
+original_Xtest = original_Xtest.values
+original_ytrain = original_ytrain.values
+original_ytest = original_ytest.values
+
+# See if both the train and test label distribution are similarly distributed
+train_unique_label, train_counts_label = np.unique(original_ytrain, return_counts=True)
+test_unique_label, test_counts_label = np.unique(original_ytest, return_counts=True)
+print('-' * 100)
+
+print('Label Distributions: \n')
+print(train_counts_label/ len(original_ytrain))
+print(test_counts_label/ len(original_ytest))
+
+# %%
+from collections import Counter
+from imblearn.under_sampling import NearMiss
+from imblearn.pipeline import make_pipeline as imbalanced_make_pipeline
+from imblearn.metrics import classification_report_imbalanced
+
+# We will undersample during cross validating
+undersample_X = new_df.drop('target', axis=1)
+undersample_y = new_df['target']
+
+
+for train_index, test_index in sss.split(undersample_X, undersample_y):
+    print("Train:", train_index, "Test:", test_index)
+    undersample_Xtrain, undersample_Xtest = undersample_X.iloc[train_index], undersample_X.iloc[test_index]
+    undersample_ytrain, undersample_ytest = undersample_y.iloc[train_index], undersample_y.iloc[test_index]
+    
+undersample_Xtrain = undersample_Xtrain.values
+undersample_Xtest = undersample_Xtest.values
+undersample_ytrain = undersample_ytrain.values
+undersample_ytest = undersample_ytest.values 
+
+undersample_accuracy = []
+undersample_precision = []
+undersample_recall = []
+undersample_f1 = []
+undersample_auc = []
+
+# Implementing NearMiss Technique 
+# Distribution of NearMiss (Just to see how it distributes the labels we won't use these variables)
+X_nearmiss, y_nearmiss = NearMiss().fit_sample(undersample_X.values, undersample_y.values)
+print('NearMiss Label Distribution: {}'.format(Counter(y_nearmiss)))
+# Cross Validating the right way
+
+for train, test in sss.split(undersample_Xtrain, undersample_ytrain):
+    undersample_pipeline = imbalanced_make_pipeline(NearMiss(sampling_strategy='majority'), log_reg) # SMOTE happens during Cross Validation not before..
+    undersample_model = undersample_pipeline.fit(undersample_Xtrain[train], undersample_ytrain[train])
+    undersample_prediction = undersample_model.predict(undersample_Xtrain[test])
+    
+    undersample_accuracy.append(undersample_pipeline.score(original_Xtrain[test], original_ytrain[test]))
+    undersample_precision.append(precision_score(original_ytrain[test], undersample_prediction))
+    undersample_recall.append(recall_score(original_ytrain[test], undersample_prediction))
+    undersample_f1.append(f1_score(original_ytrain[test], undersample_prediction))
+    try:
+        undersample_auc.append(roc_auc_score(original_ytrain[test], undersample_prediction))
+    except ValueError as e:
+        undersample_auc.append(undersample_auc[-1])
+#%%
+
+
+# %%
+# Let's Plot LogisticRegression Learning Curve
+from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import learning_curve
+
+def plot_learning_curve(estimator1, estimator2, estimator3, estimator4, X, y, ylim=None, cv=None,
+                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
+    f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2,2, figsize=(20,14), sharey=True)
+    if ylim is not None:
+        plt.ylim(*ylim)
+    # First Estimator
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator1, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax1.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax1.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax1.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax1.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax1.set_title("Logistic Regression Learning Curve", fontsize=14)
+    ax1.set_xlabel('Training size (m)')
+    ax1.set_ylabel('Score')
+    ax1.grid(True)
+    ax1.legend(loc="best")
+    
+    # Second Estimator 
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator2, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax2.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax2.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax2.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax2.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax2.set_title("Knears Neighbors Learning Curve", fontsize=14)
+    ax2.set_xlabel('Training size (m)')
+    ax2.set_ylabel('Score')
+    ax2.grid(True)
+    ax2.legend(loc="best")
+    
+    # Third Estimator
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator3, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax3.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax3.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax3.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax3.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax3.set_title("Support Vector Classifier \n Learning Curve", fontsize=14)
+    ax3.set_xlabel('Training size (m)')
+    ax3.set_ylabel('Score')
+    ax3.grid(True)
+    ax3.legend(loc="best")
+    
+    # Fourth Estimator
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator4, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    ax4.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="#ff9124")
+    ax4.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="#2492ff")
+    ax4.plot(train_sizes, train_scores_mean, 'o-', color="#ff9124",
+             label="Training score")
+    ax4.plot(train_sizes, test_scores_mean, 'o-', color="#2492ff",
+             label="Cross-validation score")
+    ax4.set_title("Decision Tree Classifier \n Learning Curve", fontsize=14)
+    ax4.set_xlabel('Training size (m)')
+    ax4.set_ylabel('Score')
+    ax4.grid(True)
+    ax4.legend(loc="best")
+    return plt
+
+# %%
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=42)
+plot_learning_curve(log_reg, knears_neighbors, svc, tree_clf, X_train, y_train, (0.87, 1.01), cv=cv, n_jobs=4)
+
+# %%
