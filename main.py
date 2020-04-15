045776f8b5ec1a3c5b2332206ca1ee93452d0ae6
liziwl <leezisy@gmail.com>
2020-04-15 09:34:14 +0800

    update

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # %%
-from sklearn.decomposition import PCA
-from sklearn.model_selection import GridSearchCV, KFold, cross_val_score
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV, KFold
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
-from sklearn.metrics import auc, classification_report, f1_score, precision_score, recall_score, roc_auc_score, roc_curve
 
 train_data_ = pd.read_csv('dataset/train.data', sep='\s+', header=None)
 test_data_ = pd.read_csv('dataset/test.data', sep='\s+', header=None)
@@ -20,12 +20,13 @@ target_count = test_data_.target.value_counts()
 print('Class -1:', target_count[-1])
 print('Class 1:', target_count[1])
 print('Proportion:', round(target_count[-1] / target_count[1], 2), ': 1')
-print(f'Rate: {target_count[-1]/(target_count[-1] + target_count[1])*100}%')
+print(f'Rate: {target_count[-1] / (target_count[-1] + target_count[1]) * 100}%')
 
 ax = target_count.plot(kind='bar', title='Count (target Class)', rot=0)
 ax.set_ylim([0, 2200])
 for p in ax.patches:
-    ax.annotate(np.round(p.get_height(), decimals=2), (p.get_x()+p.get_width()/2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+    ax.annotate(np.round(p.get_height(), decimals=2), (p.get_x() + p.get_width() / 2., p.get_height()), ha='center',
+                va='center', xytext=(0, 10), textcoords='offset points')
 plt.savefig('test_dist.png', bbox_inches='tight', dpi=400, pad_inches=0.05)
 
 # %%
@@ -34,12 +35,13 @@ target_count = train_data_.target.value_counts()
 print('Class -1:', target_count[-1])
 print('Class 1:', target_count[1])
 print('Proportion:', round(target_count[-1] / target_count[1], 2), ': 1')
-print(f'Rate: {target_count[-1]/(target_count[-1] + target_count[1])*100}%')
+print(f'Rate: {target_count[-1] / (target_count[-1] + target_count[1]) * 100}%')
 
 ax = target_count.plot(kind='bar', title='Count (target Class)', rot=0)
 ax.set_ylim([0, 8500])
 for p in ax.patches:
-    ax.annotate(np.round(p.get_height(), decimals=2), (p.get_x()+p.get_width()/2., p.get_height()), ha='center', va='center', xytext=(0, 10), textcoords='offset points')
+    ax.annotate(np.round(p.get_height(), decimals=2), (p.get_x() + p.get_width() / 2., p.get_height()), ha='center',
+                va='center', xytext=(0, 10), textcoords='offset points')
 plt.savefig('train_dist.png', bbox_inches='tight', dpi=400, pad_inches=0.05)
 
 # %%
@@ -52,7 +54,7 @@ df_class_1 = train_data_[train_data_.target == 1]
 
 # %%
 
-# 欠采样
+# 欠采样（丢失数据不好）
 df_class_0_under = df_class_0.sample(count_class_1)
 df_test_under = pd.concat([df_class_0_under, df_class_1], axis=0)
 
@@ -93,8 +95,10 @@ rfc = RandomForestClassifier(n_estimators=100, random_state=42)  # criterion = e
 
 # models.sort_values(by='Score', ascending=False)
 
-rfc_grid = [{'n_estimators': [3, 10, 15, 40, 50, 60, 70, 80, 90, 100, 120, 150, 170, 200], 'max_depth':[1, 2, 3, 4, 5, 6, 7, None]},
-            {'bootstrap': [False], 'n_estimators':[3, 10, 15, 40, 50, 60, 70, 80, 90, 100, 120, 150, 170, 200], 'max_depth':[1, 2, 3, 4, 5, 6, 7, None]}]
+rfc_grid = [{'n_estimators': [3, 10, 15, 40, 50, 60, 70, 80, 90, 100, 120, 150, 170, 200],
+             'max_depth': [1, 2, 3, 4, 5, 6, 7, None]},
+            {'bootstrap': [False], 'n_estimators': [3, 10, 15, 40, 50, 60, 70, 80, 90, 100, 120, 150, 170, 200],
+             'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}]
 rfc_gs = GridSearchCV(estimator=rfc, param_grid=rfc_grid, cv=10, scoring='f1', n_jobs=-1)
 rfc_gs.fit(over_x, over_y)
 # rfc_f1_scores = cross_val_score(estimator=rfc_gs, X=over_x, y=over_y, scoring='f1', cv=5)
@@ -107,7 +111,7 @@ print(rfc_gs.best_score_)
 
 # %%
 
-# 测试精度train
+# 精度 train
 test_x = train_data_.drop('target', axis=1)
 test_y = train_data_['target']
 
@@ -115,14 +119,13 @@ y_pred = rfc_gs.predict(test_x)
 print("TRAIN")
 print(classification_report(test_y, y_pred))
 
-# 测试精度test
+# 精度 test
 test_x = test_data_.drop('target', axis=1)
 test_y = test_data_['target']
 
 y_pred = rfc_gs.predict(test_x)
 print("TEST")
 print(classification_report(test_y, y_pred))
-
 
 # %%
 
@@ -136,23 +139,11 @@ pca.fit(X)
 print(pca.explained_variance_ratio_)
 print(pca.explained_variance_)
 
-
 # %%
 
 X_new = pca.transform(X)
 plt.scatter(X_new[:, 0], X_new[:, 1], marker='o', c=train_data_['target'])
 plt.show()
-
-# %%
-new_df = df_test_under.sample(frac=1, random_state=42)
-new_df.head()
-
-# %%
-
-X = new_df.drop('target', axis=1)
-y = new_df['target']
-
-y
 
 # %%
 train_data_.iloc[:, 6].value_counts()
