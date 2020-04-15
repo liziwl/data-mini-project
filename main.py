30429fcbb4905adc6da2e16e51b18a61b3f756f0
liziwl <leezisy@gmail.com>
2020-04-15 10:44:46 +0800

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
-from sklearn.model_selection import GridSearchCV, KFold
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 
 train_data_ = pd.read_csv('dataset/train.data', sep='\s+', header=None)
@@ -13,6 +13,13 @@ test_data_ = pd.read_csv('dataset/test.data', sep='\s+', header=None)
 
 train_data_.rename(columns={10: 'target'}, inplace=True)
 test_data_.rename(columns={10: 'target'}, inplace=True)
+
+
+def split_xy(data):
+    _x = data.drop('target', axis=1)
+    _y = data['target']
+    return _x, _y
+
 
 # %%
 # test 数据分布
@@ -77,62 +84,46 @@ df_test_over.target.value_counts().plot(kind='bar', title='Count (target)')
 # %%
 # RF
 
-over_x = df_test_over.drop('target', axis=1)
-over_y = df_test_over['target']
+over_x, over_y = split_xy(df_test_over)
 
 X_train, X_test, y_train, y_test = train_test_split(over_x, over_y, test_size=0.2, random_state=42)
-k_fold = KFold(n_splits=10, shuffle=True, random_state=0)
 
 # %%
 rfc = RandomForestClassifier(n_estimators=100, random_state=42)  # criterion = entopy,gini
-# rfc.fit(X_train, y_train)
-# rfcpred = rfc.predict(X_test)
-# RFCCV = (cross_val_score(rfc, X_train, y_train, cv=k_fold, scoring = 'accuracy').mean())
 
-# models = pd.DataFrame({
-#                 'Models': ['Random Forest Classifier'],
-#                 'Score':  [RFCCV]})
-
-# models.sort_values(by='Score', ascending=False)
-
-rfc_grid = [{'n_estimators': [3, 10, 15, 40, 50, 60, 70, 80, 90, 100, 120, 150, 170, 200],
+n_est = [3, 10, 15, 40, 50, 60]
+rfc_grid = [{'n_estimators': n_est,
              'max_depth': [1, 2, 3, 4, 5, 6, 7, None]},
-            {'bootstrap': [False], 'n_estimators': [3, 10, 15, 40, 50, 60, 70, 80, 90, 100, 120, 150, 170, 200],
+            {'bootstrap': [False], 'n_estimators': n_est,
              'max_depth': [1, 2, 3, 4, 5, 6, 7, None]}]
 rfc_gs = GridSearchCV(estimator=rfc, param_grid=rfc_grid, cv=10, scoring='f1', n_jobs=-1)
 rfc_gs.fit(over_x, over_y)
 # rfc_f1_scores = cross_val_score(estimator=rfc_gs, X=over_x, y=over_y, scoring='f1', cv=5)
 # print('CV clf f1 score:%.3f +/- %.3f' % (np.mean(rfc_f1_scores), np.std(rfc_f1_scores)))
 
-print(rfc_gs.best_params_)
-# {'bootstrap': False, 'max_depth': None, 'n_estimators': 40}
-print(rfc_gs.best_score_)
-# 0.9836533023676681
+print(rfc_gs.best_params_)  # {'bootstrap': False, 'max_depth': None, 'n_estimators': 10}
+print(rfc_gs.best_score_)  # 0.9836533023676681
+
 
 # %%
+
+def print_report(data_name, clf_name, clf, x_test, y_real):
+    print("-" * 20 + f"{data_name}@{clf_name}" + "-" * 20)
+    y_pred = clf.predict(x_test)
+    print(classification_report(y_real, y_pred))
+
 
 # 精度 train
-test_x = train_data_.drop('target', axis=1)
-test_y = train_data_['target']
-
-y_pred = rfc_gs.predict(test_x)
-print("TRAIN")
-print(classification_report(test_y, y_pred))
+test_x, test_y = split_xy(train_data_)
+print_report('TRAIN', 'RF', rfc_gs, test_x, test_y)
 
 # 精度 test
-test_x = test_data_.drop('target', axis=1)
-test_y = test_data_['target']
-
-y_pred = rfc_gs.predict(test_x)
-print("TEST")
-print(classification_report(test_y, y_pred))
+test_x, test_y = split_xy(test_data_)
+print_report('TEST', 'RF', rfc_gs, test_x, test_y)
 
 # %%
 
-X = train_data_.drop('target', axis=1)
-X
-
-# %%
+X, Y = split_xy(train_data_)
 pca = PCA(n_components=10)
 pca.fit(X)
 
